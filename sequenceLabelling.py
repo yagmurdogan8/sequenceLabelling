367f40cd5ca0319680097e7af5838391a4c6330a
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-11-03 21:06:10 +0300

    y

--- sequenceLabelling.py
+++ sequenceLabelling.py
@@ -1,5 +1,5 @@
 from datasets import Dataset, DatasetDict
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, DataCollatorForTokenClassification
 
 sentences = []
 
@@ -149,95 +149,10 @@ tokenized_dataset = train_dataset.map(
     batched=True,
     remove_columns=dataset["train"].column_names,
 )
-#
-#
-# def read_from_wnut_file(path):
-#     train_data = []
-#     with open(path, 'r', encoding='utf-8') as file:
-#         for line in file:
-#             line = line.strip()
-#             if line:
-#                 tokens, label = line[0], line[1]
-#                 train_data.append({"tokens": tokens, "labels": label})
-#
-#     return train_data
-#
-#
-# wnut_file = 'data/wnut17train.conll'
-# sentences = read_from_wnut_file(wnut_file)
-#
-#
-# def ids_tokens_nertags(sentences):
-#     res_ids = []
-#     res_tokens = []
-#     res_ner_tags = []
-#     for id, token_ner_tags in enumerate(sentences):
-#         res_ids.append(id)
-#         tokens, ner_tags = zip(*token_ner_tags)
-#         res_tokens.append(list(tokens))
-#         res_ner_tags.append(list(ner_tags))
-#
-#     return res_ids, res_tokens, res_ner_tags
-#
-#
-# ids, tokens, ner_tags_str = ids_tokens_nertags(sentences)
-# #
-# # train_data = []
-# #
-# #
-# # def read_wnut():
-# #     with open("data/wnut17train.conll", "r", encoding='utf-8') as train_file:
-# #         for line in train_file:
-# #             line = line.strip().split()
-# #             if line:
-# #                 tokens, label = line[0], line[1]
-# #                 train_data.append({"tokens": tokens, "labels": label})
-# #
-# #     return train_data
-# #
-# #
-# # file_path = "data/wnut17train.conll"
-# #
-# # tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-# #
-# #
-# # # def ids_tokens_nertags(sentences):
-# # #     res_ids = []
-# # #     res_tokens = []
-# # #     res_ner_tags = []
-# # #     for id, token_ner_tags in enumerate(sentences):
-# # #         res_ids.append(id)
-# # #         tokens, ner_tags = zip(*token_ner_tags)
-# # #         res_tokens.append(list(tokens))
-# # #         res_ner_tags.append(list(ner_tags))
-# # #
-# # #     return res_ids, res_tokens, res_ner_tags
-# # #
-# # #
-# # # ids, tokens, ner_tags_str = ids_tokens_nertags(sentences)
-# #
-# #
-# # def tokenize_and_preprocess_data(example):
-# #     tokens = example['tokens']
-# #     labels = example['labels']
-# #
-# #     # Tokenize the text
-# #     tokenized_input = tokenizer(tokens, is_split_into_words=True)
-# #
-# #     # Ensure that the tokenized input matches the token boundaries
-# #     assert len(tokenized_input['input_ids']) == len(tokens)
-# #
-# #     return {
-# #         'input_ids': tokenized_input['input_ids'],
-# #         'attention_mask': tokenized_input['attention_mask'],
-# #         'labels': labels,
-# #     }
-# #
-# #
-# # # preprocess data
-# # train_data = [tokenize_and_preprocess_data(example) for example in train_data]
-# #
-# # # ner_feature = train_data[0]["labels"]
-# # # print(ner_feature)
-# #
-# # print(train_data)
+
+# Fine tuning
+
+data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+
+batch = data_collator([tokenized_dataset["train"][i] for i in range(2)])
+print(batch["labels"])
