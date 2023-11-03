d023c5dc26cf818bdde1ae1cda3024127192d755
Yağmur DOĞAN <85808317+yagmurdogan8@users.noreply.github.com>
2023-11-03 16:47:45 +0300

    y

---
+++ sequenceLabelling.py
@@ -0,0 +1,50 @@
+from transformers import AutoTokenizer
+
+train_data = []
+with open("data/wnut17train.conll", "r", encoding='utf-8') as train_file:
+    for line in train_file:
+        line = line.strip().split()
+        if line:
+            tokens, label = line[0], line[1]
+            train_data.append({"tokens": tokens, "labels": label})
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+def ids_tokens_nertags(sentences):
+    res_ids = []
+    res_tokens = []
+    res_ner_tags = []
+    for id,token_ner_tags in enumerate(sentences):
+      res_ids.append(id)
+      tokens, ner_tags = zip(*token_ner_tags)
+      res_tokens.append(list(tokens))
+      res_ner_tags.append(list(ner_tags))
+
+    return res_ids, res_tokens, res_ner_tags
+
+ids, tokens, ner_tags_str = ids_tokens_nertags(sentences)
+def tokenize_and_preprocess_data(example):
+    tokens = example['tokens']
+    labels = example['labels']
+
+    # Tokenize the text
+    tokenized_input = tokenizer(tokens, is_split_into_words=True)
+
+    # Ensure that the tokenized input matches the token boundaries
+    assert len(tokenized_input['input_ids']) == len(tokens)
+
+    return {
+        'input_ids': tokenized_input['input_ids'],
+        'attention_mask': tokenized_input['attention_mask'],
+        'labels': labels,
+    }
+
+
+# preprocess data
+train_data = [tokenize_and_preprocess_data(example) for example in train_data]
+
+ner_feature = train_data[0]["labels"]
+print(ner_feature)
+
+
+# print(train_data)
